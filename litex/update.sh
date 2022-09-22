2b0faf0e8c092444b6a271fd0ab095608ae8453a
Qiang Du <QDu@lbl.gov>
2022-09-22 16:44:09 -0700

    Update Makefile for little endian conversion due to litex 2022.08

---
+++ litex/update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# scp du@qdu.dhcp.lbl.gov:~/test/uspas_llrf/litex/build/berkeleylab_marble/gateware/digilent_arty.bit build/digilent_arty/gateware/digilent_arty.bit
+mkdir -p build/berkeleylab_marble/gateware/
+scp du@qdu.dhcp.lbl.gov:~/test/uspas_llrf/litex/build/berkeleylab_marble/gateware/berkeleylab_marble.bit build/berkeleylab_marble/gateware/berkeleylab_marble.bit
