2996252c1a89b59a6ca5a434835bd9e213eb1347
Qiang Du <QDu@lbl.gov>
2022-09-23 18:29:42 -0700

    Update .gitignore

--- litex/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# scp du@qdu.dhcp.lbl.gov:~/test/uspas_llrf/litex/build/berkeleylab_marble/gateware/digilent_arty.bit build/digilent_arty/gateware/digilent_arty.bit
-mkdir -p build/berkeleylab_marble/gateware/
-scp du@qdu.dhcp.lbl.gov:~/test/uspas_llrf/litex/build/berkeleylab_marble/gateware/berkeleylab_marble.bit build/berkeleylab_marble/gateware/berkeleylab_marble.bit
-scp du@qdu.dhcp.lbl.gov:~/test/uspas_llrf/litex/build/berkeleylab_marble/gateware/berkeleylab_marble.bin build/berkeleylab_marble/gateware/berkeleylab_marble.bin
