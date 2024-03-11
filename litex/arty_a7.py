3f17cab2797890d46b856d7e453a033747840b90
Qiang Du <QDu@lbl.gov>
2024-03-11 14:37:36 -0700

    Remove litex directory

--- litex/arty_a7.py
+++
@@ -1,4 +0,0 @@
-from litex_boards.targets.digilent_arty import main
-
-if __name__ == "__main__":
-    main()
--- litex/marble.py
+++
@@ -1,5 +0,0 @@
-# from litex_boards.targets.berkeleylab_marble import main
-from targets.marble import main
-
-if __name__ == "__main__":
-    main()
