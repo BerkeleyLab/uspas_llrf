83808c40f7cadeb3f77363db0b56a48c29cc7475
Qiang Du <QDu@lbl.gov>
2024-05-31 18:37:22 -0700

    Add llrf_dsp/tests/check_results.py to pass cocotb test failures out

---
+++ llrf_dsp/tests/check_results.py
@@ -0,0 +1,14 @@
+from xml.etree import ElementTree as ET
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--result-fname", default="results.xml")
+    args = parser.parse_args()
+
+    result = ET.parse(args.result_fname)
+    for testsuite in result.iter("testsuite"):
+        for testcase in testsuite.iter("testcase"):
+            for failure in testcase.iter("failure"):
+                raise Exception(f'{testcase.get("name")}: Test Failed.')
