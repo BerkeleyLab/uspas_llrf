74cdfb4a8c47ce2e37f0f6067e08352ff29df181
Qiang Du <QDu@lbl.gov>
2024-06-17 21:49:25 -0700

    Update zest_dac_interp.v to allow general interpolation

--- llrf_dsp/zest_dac_interp.v
+++ llrf_dsp/zest_dac_interp.v
@@ -2,26 +2,39 @@ module zest_dac_interp #(
     parameter integer DW=14
 ) (
     input dsp_clk,
-    input [DW-1:0] data_dsp_in,
-    input [DW-1:0] coeff,
+    input signed [DW-1:0] din,
+    input signed [DW-1:0] coeff,
     input dac_clk,
-    output [DW-1:0] data_dac_out
+    output signed [DW-1:0] dout
 );
+    // input is: s0, s1, s2, ...
     // dac_clk is 2x of dsp_clk, phase aligned
-    (* ASYNC_REG = "TRUE" *) reg signed [DW-1:0] d0=0, d1=0;
+    wire signed [DW-1:0] d1;
+    data_xdomain #(.size(DW)) data_xdomain (
+        .clk_in         (dsp_clk),
+        .gate_in        (1'b1),
+        .data_in        (din),
+        .clk_out        (dac_clk),
+        .gate_out       (),
+        .data_out       (d1)
+    );
+
+    reg signed [DW-1:0] d2=0, d3=0, d4=0;
+    reg tick=0;
     always @(posedge dac_clk) begin
-        d0 <= data_dsp_in;
-        d1 <= d0;
+        tick <= ~tick;
+        d2 <= d1;
+        d3 <= d2;
+        d4 <= d3;
     end
 
-    reg signed [2*DW:0] r=0;
-    reg signed [DW-1:0] d2=0, r1=0;
     wire signed [DW:0] sum = d2 + d1;
+    reg signed [2*DW:0] r=0, r1=0;
     always @(posedge dac_clk) begin
-        d2 <= d1;
         r <= sum * coeff;
         r1 <= r >>> DW;
     end
-    assign data_dac_out = r1;
+    // output is: s0, (s0+s1)*coeff, s1, (s1+s2)*coeff, s2, ...
+    assign dout = tick ? r1[DW-1:0] : d4;
 
 endmodule
--- llrf_dsp/zest_dac_interp_tb.v
+++ llrf_dsp/zest_dac_interp_tb.v
@@ -3,7 +3,8 @@
 
 module zest_dac_interp_tb;
 parameter integer DW=14;
-parameter real coeff_r=0.5;     // adjust me
+parameter real coeff_r=0.3;     // adjust me. 0.5 for normal interp.
+parameter integer LATENCY=8;
 
 integer cc=0;
 reg dac_clk=1, dsp_clk=1;
@@ -13,7 +14,10 @@ initial begin
         $dumpfile("zest_dac_interp.vcd");
         $dumpvars(5,zest_dac_interp_tb);
     end
-    for (cc=0; cc<=11; cc=cc+1) begin
+    $display("%4s %8s %8s %8s %6s",
+        "cc", "data_dsp", "data_dac", "expected", "result");
+    $display("--------------------------------------");
+    for (cc=0; cc<=21; cc=cc+1) begin
         dac_clk = 1; #(`DSP_CLK_CYCLE/4);
         dac_clk = 0; #(`DSP_CLK_CYCLE/4);
     end
@@ -28,16 +32,16 @@ wire signed [DW-1:0] data_dac;
 
 zest_dac_interp #(.DW(DW)) dut(
     .dsp_clk        (dsp_clk),
-    .data_dsp_in    (data_dsp),
+    .din            (data_dsp),
     .coeff          (coeff),
     .dac_clk        (dac_clk),
-    .data_dac_out   (data_dac)
+    .dout           (data_dac)
 );
 
 // Verification logic
 // find expectations
 wire signed [DW-1:0] data_dsp_delay;
-reg_delay #(.dw(DW), .len(5)) delay1 (
+reg_delay #(.dw(DW), .len(LATENCY)) delay1 (
     .clk    (dac_clk),
     .reset  (1'b0),
     .gate   (1'b1),
@@ -50,25 +54,26 @@ always @(posedge dsp_clk) d1 <= data_dsp;
 wire signed [DW-1:0] data_interp = (d1 + data_dsp) * coeff_r;
 wire signed [DW-1:0] data_interp_delay;
 
-reg_delay #(.dw(DW), .len(4)) delay2 (
+reg_delay #(.dw(DW), .len(LATENCY-1)) delay2 (
     .clk    (dac_clk),
     .reset  (1'b0),
     .gate   (1'b1),
     .din    (data_interp),
     .dout   (data_interp_delay)
 );
-wire sel = cc % 2 == 0;
-wire signed [DW-1:0] data_exp = sel ? data_interp_delay : data_dsp_delay;
+wire signed [DW-1:0] data_exp = dut.tick ? data_interp_delay : data_dsp_delay;
 
 initial begin
     @(posedge dsp_clk) data_dsp <= 200;
     @(posedge dsp_clk) data_dsp <= 400;
     @(posedge dsp_clk) data_dsp <= -500;
+    @(posedge dsp_clk) data_dsp <= 700;
 end
 
+wire signed [DW-1:0] err = data_dac - data_exp;
 always @(negedge dac_clk) begin
-    if (cc >=8 ) pass &= (data_dac == data_exp);
-    $display("cc=%4d, data_dsp = %5d, data_dac = %5d, data_exp = %5d, %s",
+    if (cc >=LATENCY+2 ) pass &= $abs(err) <= 1;
+    $display("%4d %8d %8d %8d %6s",
         cc, data_dsp, data_dac, data_exp, pass?"PASS":"FAIL");
 end
 
