273f80847c2cdbac039637f5869f376206d1b85f
Qiang Du <QDu@lbl.gov>
2024-07-10 18:44:06 -0700

    Enable amplitude / phase plot from circular buffer data in python/llrf_app

--- python/llrf_app/app.py
+++ python/llrf_app/app.py
@@ -5,13 +5,13 @@ import json
 import time
 import matplotlib.animation as animation
 import logging
-np.set_printoptions(precision=0, suppress=True, edgeitems=4, linewidth=120)
 logger = logging.getLogger(__name__)
 
 
 class LLRFApp(LEEPDevice):
     def __init__(self, addr='192.168.19.42:803', conf='LEMP',
                  settings_fname='../llrf_dsp/settings.json',
+                 chan_keep=0x3ff, wfm_len=4096,
                  timeout=0.1, **kwargs):
         self.init_rom_addr = 0x08000
         super(LLRFApp, self).__init__(addr, timeout, **kwargs)
@@ -20,14 +20,13 @@ class LLRFApp(LEEPDevice):
             configs = json.load(f)
         for k, v in configs[conf].items():
             setattr(self, k.lower(), v)
-        self.cordic_gain = 1.646760258
         self.wave_samp_per = 1
+        self.wfm_len = wfm_len
+        assert self.wfm_len <= 2**15  # cbuf size / 2
         self.ts = self.dsp_clk_cycle * self.cic_base_period
         self.ts *= self.wave_samp_per
-        self.chan_keep = 0x03ff  # 2 dacs, 8 adcs
-        self.adc_len = 4096
+        self.chan_keep = chan_keep & 0x03ff  # 2 dacs, 8 adcs
         self.n_chan = bin(self.chan_keep).count('1')
-        self.wfm_len = 4096
         self.adc_names = [f'ADC {i}' for i in range(8)]
         self.dac_names = ['DAC 0', 'DAC 1']
         self.chan_names = self.adc_names + self.dac_names
@@ -40,10 +39,10 @@ class LLRFApp(LEEPDevice):
             ('wave_samp_per', self.wave_samp_per),
             ('chan_keep', self.chan_keep)
         ])
-        # print(f'chan_keep: {self.chan_keep:#018b}')
+        logger.debug(f'chan_keep: {self.chan_keep:#018b}')
         self.chans = np.where(
             np.array([int(x) for x in f'{self.chan_keep:b}'[::-1]]) == 1)[0]
-        # print(f'chans selected: {self.chans}')
+        logger.debug(f'chans selected: {self.chans}')
 
     def read_reg(self, name):
         ''' read single register by given name '''
@@ -52,10 +51,6 @@ class LLRFApp(LEEPDevice):
     def write_reg(self, name, val):
         ''' read single register by given name '''
         return self.reg_write([(name, val)])
-
-    def decode_twos_comp(self, darray, bits=16):
-        mask = 2**(bits - 1)
-        return -np.bitwise_and(darray, mask) + np.bitwise_and(darray, ~mask)
 
     def read_adc_bufs(self):
         return np.array(self.reg_read([
@@ -67,43 +62,26 @@ class LLRFApp(LEEPDevice):
             'dac' + str(chan) + '_buf' for chan in range(2)
             ]), dtype=np.int16)
 
-    def update_plot(self, darray):
-        for line, ydata in zip(self.lines, darray):
-            line.set_ydata(ydata)
-        return self.lines
-
     def read_cbuf_data(self):
         self.write_reg('circle_buf_flip', 1)
-        while (self.read_reg('llrf_circle_ready') & 1 != 1):
+        while (self.read_reg('llrf_circle_ready') != 3):
             time.sleep(0.01)
         d = np.array(self.read_reg('circle_data'))
-        return d[:self.wfm_len * self.n_chan]
+        return d[:self.wfm_len * 2 * self.n_chan]
 
-    def get_cbuf_wfm(self):
-        yield self.read_cbuf_data()
+    def get_mp_wfm(self):
+        darray = self.read_cbuf_data()
+        iq_traces = self.calc_iq_arrays(darray)
+        return self.calc_mp_traces(iq_traces)
 
-    def update_cbuf_wfm(self, varray):
-        darray = varray.reshape(-1, self.n_chan).T
-        for line, ydata in zip(self.lines, darray):
-            line.set_ydata(ydata)
-        return self.lines
+    def calc_iq_arrays(self, varray):
+        darray = varray.reshape(-1, 2*self.n_chan).T
+        iq_arrays = np.array([
+            (darray[ix*2] + 1j * darray[ix*2+1])
+            for ix in range(self.n_chan)]) / self.amp_rx_gain
+        return iq_arrays
 
-    def plot_cbuf_wfm(self):
-        self.fig, self.axes = plt.subplots(
-            nrows=self.n_chan, figsize=(10, 10), sharex=True, squeeze=True)
-        self.fig.suptitle('adc chan: {}'.format(self.chans))
-
-        self.lines = [ax.plot(
-            np.arange(self.adc_len),
-            np.zeros(self.adc_len), animated=True)[0] for ax in self.axes]
-
-        for ix, ax in enumerate(self.axes):
-            ax.set_ylim(-1 << 15, 1 << 15)
-            ax.set_ylabel(self.chan_names[self.chans[ix]])
-            ax.grid(color='grey', alpha=.5, linestyle='--')
-        self.axes[-1].set_xlabel('#Trigger')
-        # self.axes[0].autoscale(axis='y')
-
-        self.anim = animation.FuncAnimation(
-            self.fig, self.update_cbuf_wfm, self.get_cbuf_wfm,
-            interval=5, blit=True, save_count=50)
+    def calc_mp_traces(self, iq_arrays):
+        mag_trace = np.abs(iq_arrays)
+        phs_trace = np.angle(iq_arrays, deg=True)
+        return np.vstack((mag_trace, phs_trace))
