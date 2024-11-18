7175f70e1c232751e9c82ba1bcbd36034069a02b
Shreeharshini Murthy <sdmurthy@lbl.gov>
2024-11-18 13:37:09 -0800

    Misc maintenance

--- top/marble_zest/marble_zest_top.tcl
+++ top/marble_zest/marble_zest_top.tcl
@@ -1,33 +1,26 @@
 set outputDir ./_xilinx
 file mkdir $outputDir
 
-# experimental!
-proc get_gitid {} {
-    switch -glob -- [exec git describe --always --dirty] {
-        *-dirty      {return [string toupper [exec git rev-parse --short=24 HEAD]]0000000000000000}
-        default      {return [string toupper [exec git rev-parse HEAD]]}
-    }
+# only source swap_gitid.tcl
+if {[llength $argv] >= 5} {
+    set aux_tcl [lindex $argv 4]
+    puts "Sourcing $aux_tcl"
+    source $aux_tcl
 }
-proc get_gitid_name {} {
-    switch -glob -- [exec git describe --always --dirty] {
-        *-dirty      {return [exec git rev-parse --short=8 HEAD]-dirty}
-        default      {return [exec git rev-parse --short=8 HEAD]}
-    }
-}
-# Print git hash in huge and nice way
-proc gitid_print {gitid_arg} {
-    puts "#[string repeat "-" 48]\n# gitid $gitid_arg\n#[string repeat "-" 48]"
-}
+
+# git context information
+array set git_status [get_git_context]
+
 # this old_commit value matches that in build_rom.py --placeholder_rev
 set old_commit [string toupper "da39a3ee5e6b4b0d3255bfef95601890afd80709"]
-set new_commit [get_gitid]
-gitid_print $new_commit
+set new_commit $git_status(full_id)
+git_id_print $new_commit
 
 # Read in dependencies file
 set flist [lindex $argv 0]
 puts "Obtaining dependencies from $flist"
 
-# Read in fset identifier
+# Read in FSET identifier
 set fset [lindex $argv 1]
 puts "Building for $fset"
 
@@ -44,13 +37,13 @@ puts "Obtaining $verilog_defines"
 set part "xc7k160tffg676-2"
 puts "Synthesizing for part $part"
 
-create_project marble_zest_top $outputDir -part $part -force
+create_project marble_zest_top_$fset $outputDir -part $part -force
 
-# Provision to source additional TCL scripts
-# Currently used for gtx_config.tcl and swap_gitid.tcl
-foreach aux_tcl [lrange $argv 4 end] {
-   puts "Sourcing $aux_tcl"
-   source $aux_tcl
+# now source gtx_config.tcl
+if {[llength $argv] >= 5} {
+    set aux_tcl [lindex $argv 5]
+    puts "Sourcing $aux_tcl"
+    source $aux_tcl
 }
 
 # Read in sources
@@ -85,7 +78,7 @@ add_files $flist_work
 set_property  top "marble_zest_top" [current_fileset]
 
 # Get shorter git commit ID for bitfile filename
-set gitid_for_filename [get_gitid_name]
+set gitid_for_filename $git_status(short_id)$git_status(suffix)
 # Disabled at least for now
 # set gitid_v 32'h$gitid
 # set new_defs [list "GIT_32BIT_ID=$gitid_v" "REVC_1W"]
@@ -128,12 +121,13 @@ proc project_rpt {dest_dir} {
 }
 
 # Report error on failed timing
-set dest_dir "$outputDir/marble_zest_top.runs"
+set dest_dir "$outputDir/marble_zest_top_$fset.runs"
 project_rpt $dest_dir
 
 # See bedrock for explanation
 swap_gitid $old_commit $new_commit 16 0
-swap_gitid $old_commit $new_commit 8 0
 
-write_bitstream -force -bin_file marble_zest_top.$gitid_for_filename.bit
-write_bitstream -force marble_zest_top.$gitid_for_filename.x.bit
+write_bitstream -force -bin_file marble_zest_top_$fset.$gitid_for_filename.bit
+write_bitstream -force marble_zest_top_$fset.$gitid_for_filename.x.bit
+
+apply_bit_stamp_mod marble_zest_top_$fset.$gitid_for_filename $git_status(dirty) $git_status(time)
