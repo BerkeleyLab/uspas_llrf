58435381db1dae892f16189c99eeaf2bfb35060e
Qiang Du <QDu@lbl.gov>
2025-08-05 17:14:44 -0700

    Remove llrf_dsp/regmap directory

--- llrf_dsp/build.mk
+++ llrf_dsp/build.mk
@@ -1,5 +1,6 @@
-APP_NAME    = llrf_shell
-JSON_DIR    = ./regmap
+MODULE    = llrf_shell
+
+# newad related: see bedrock/build-tools/newad_top_rules.mk
 LB_AW       = 17    # should be LB_HI
 NEWAD_ARGS += -m    # mirror
 NEWAD_ARGS_llrf_shell = -b69632  # 0x11000
@@ -11,38 +12,38 @@ VERILOG_AUTOGEN += $(AUTOGEN_DIR)/llrf_shell_auto.vh $(AUTOGEN_DIR)/addr_map_llr
 settings.vams: settings.json
 	$(PYTHON) llrf_model/llrf_dsp.py -c $(FSET) -f $< --write-verilog-header $@
 
-$(APP_NAME)_init_regs.json: settings.json
+$(MODULE)_init_regs.json: settings.json
 	$(PYTHON) llrf_model/llrf_dsp.py -c $(FSET) -f $< --write-init-reg $@
 
-$(DEPDIR)/$(APP_NAME).d: $(APP_NAME).v $(VERILOG_AUTOGEN) cordicg_b22.v
+$(DEPDIR)/$(MODULE).d: $(MODULE).v $(VERILOG_AUTOGEN) cordicg_b22.v
 	@set -e; mkdir -p $(DEPDIR); \
-	$(MAKEDEP) && (printf "$(APP_NAME)_expand.v $@: "; \
+	$(MAKEDEP) && (printf "$(MODULE)_expand.v $@: "; \
 	sort -ur $@.$$$$ | tr '\n' ' '; printf "\n" ) > $@ && rm -f $@.$$$$
 
 $(AUTOGEN_DIR)/%_expand.v: %.v $(AUTOGEN_DIR)/%_auto.vh $(AUTOGEN_DIR)/addr_map_%.vh
 	$(VERILOG) $(VFLAGS_DEP) -E -o$@ $^
 	sed -i '/^$$/d' $@
 
-$(APP_NAME).json: $(AUTOGEN_DIR)/regmap_$(APP_NAME).json $(AUTOGEN_DIR)/scalar_$(APP_NAME)_regmap.json $(JSON_DIR)/static_regmap.json
+$(MODULE).json: $(AUTOGEN_DIR)/regmap_$(MODULE).json $(AUTOGEN_DIR)/scalar_$(MODULE)_regmap.json static_regmap.json
 	$(PYTHON) $(BUILD_DIR)/merge_json.py -o $@ -i $^
 
 $(AUTOGEN_DIR)/scalar_%_regmap.json: %.v
 	mkdir -p $(AUTOGEN_DIR); \
 	$(PYTHON) $(BUILD_DIR)/reverse_json.py $< > $@
 
-$(APP_NAME)_expand.v: $(APP_NAME).v
+$(MODULE)_expand.v: $(MODULE).v
 	$(VERILOG) $(VFLAGS_DEP) -E -o $@ $(filter %.v, $^)
 
 ifneq (,$(findstring json,$(MAKECMDGOALS)))
-    -include $(DEPDIR)/$(APP_NAME).d
+    -include $(DEPDIR)/$(MODULE).d
 endif
 ifneq (,$(findstring _expand,$(MAKECMDGOALS)))
-    -include $(DEPDIR)/$(APP_NAME).d
+    -include $(DEPDIR)/$(MODULE).d
 endif
 ifeq (,$(MAKECMDGOALS))
-    -include $(DEPDIR)/$(APP_NAME).d
+    -include $(DEPDIR)/$(MODULE).d
 endif
 CLEAN += settings.vams
-CLEAN += $(APP_NAME).json $(APP_NAME)_init_regs.json
-CLEAN += $(APP_NAME)_expand.v
+CLEAN += $(MODULE).json $(MODULE)_init_regs.json
+CLEAN += $(MODULE)_expand.v
 CLEAN += cordicg_b22.v
